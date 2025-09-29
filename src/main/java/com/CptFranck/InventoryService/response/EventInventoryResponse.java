3e87be26b91d8f7aad479b8fcc6fc263667348b0
CptFranck <francois.caillet71@gmail.com>
2025-09-29 16:35:50 +0200

    update EventInventoryResponse class

--- src/main/java/com/CptFranck/InventoryService/response/EventInventoryResponse.java
+++ src/main/java/com/CptFranck/InventoryService/response/EventInventoryResponse.java
@@ -1,4 +1,17 @@
 package com.CptFranck.InventoryService.response;
 
+import com.CptFranck.InventoryService.entity.VenueEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class EventInventoryResponse {
+    private String event;
+    private Long capacity;
+    private VenueEntity venue;
 }
