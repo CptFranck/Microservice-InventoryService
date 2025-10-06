1f185383d99bd7a5991b719cfbe69c5c697420fb
CptFranck <francois.caillet71@gmail.com>
2025-10-06 17:13:25 +0200

    update Inventory controller with put entry

--- src/main/java/com/CptFranck/InventoryService/controller/InventoryController.java
+++ src/main/java/com/CptFranck/InventoryService/controller/InventoryController.java
@@ -3,6 +3,7 @@ package com.CptFranck.InventoryService.controller;
 import com.CptFranck.InventoryService.dto.EventInventoryResponse;
 import com.CptFranck.InventoryService.dto.VenueInventoryResponse;
 import com.CptFranck.InventoryService.service.InventoryService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -30,5 +31,12 @@ public class InventoryController {
     @GetMapping("/inventory/event/{eventId}")
     public @ResponseBody EventInventoryResponse inventoryForEvent(@PathVariable("eventId") Long eventId) {
         return inventoryService.getEventInventory(eventId);
+    }
+
+    @PutMapping("/inventory/event/{eventId}/capacity/{capacity}")
+    public ResponseEntity<Void> updateEventCapacity(@PathVariable("eventId") Long eventId,
+                                                    @PathVariable("capacity") Long ticketsBooked) {
+        inventoryService.updateEventCapacity(eventId, ticketsBooked);
+        return ResponseEntity.ok().build();
     }
 }
