322ae6af23e4cf9f8dc07cd2feeafb62781100f3
CptFranck <francois.caillet71@gmail.com>
2025-11-06 16:50:55 +0100

    add migration file

---
+++ src/main/resources/db/migration/V5__update_customer_id_type.sql
@@ -0,0 +1,11 @@
+ALTER TABLE `order` DROP FOREIGN KEY fk_order_customer;
+
+ALTER TABLE `order`
+    MODIFY COLUMN customer_id VARCHAR(36);
+
+ALTER TABLE `order`
+    MODIFY COLUMN customer_id VARCHAR(36);
+
+ALTER TABLE `order`
+    ADD CONSTRAINT fk_order_customer
+        FOREIGN KEY (customer_id) REFERENCES customer(id) ON DELETE SET NULL;
